ffc8bd435df4fafcf876b816c854706ba3666087
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-01-15 12:09:27 +0330

    Feature: Add Auth and counter slice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,31 @@
 import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector(state => state.auth.isAuthenticate);
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+      {isAuth && ( <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
-      </nav>
+      </nav> )}
+
     </header>
   );
 };
